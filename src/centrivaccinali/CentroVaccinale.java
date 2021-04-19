30051fdb77cb48bf3e2061f319b3b860ba84a4ad
SailenT <david.poletti3@gmail.com>
2021-04-19 16:56:54 +0200

    Creazione principali package

---
+++ src/centrivaccinali/CentroVaccinale.java
@@ -0,0 +1,8 @@
+package centrivaccinali;
+
+public class CentroVaccinale {
+
+    public static void main(String[] args){
+        
+    }
+}
--- src/cittadini/Cittadino.java
+++ src/cittadini/Cittadino.java
@@ -1,2 +1,4 @@
+package cittadini;
+
 public class Cittadino {
 }
