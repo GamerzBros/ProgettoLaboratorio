9745059da43f75b4e916253bb60f272c77a46693
SailenT <david.poletti3@gmail.com>
2021-04-19 17:11:32 +0200

    Definizione obbiettivi

--- src/centrivaccinali/CentriVaccinali.java
+++ src/centrivaccinali/CentriVaccinali.java
@@ -1,8 +1,17 @@
 package centrivaccinali;
 
 public class CentriVaccinali {
+    //TODO Definire gli attributi dei cittadini
+
+    public CentriVaccinali(){
+        createUI();
+    }
+
+    private void createUI(){
+        //TODO Crea Interfaccia Grafica Centri Vaccinali
+    }
 
     public static void main(String[] args){
-        
+
     }
 }
---
+++ src/cittadini/Cittadini.java
@@ -0,0 +1,13 @@
+package cittadini;
+
+public class Cittadini {
+    //TODO Definire gli attributi dei cittadini
+
+    public Cittadini(){
+        createUI();
+    }
+
+    private void createUI(){
+        //TODO Crea Interfaccia Grafica Cittadini
+    }
+}
