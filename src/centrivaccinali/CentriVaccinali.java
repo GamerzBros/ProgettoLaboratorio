ee0206ee645f3e40fe7f4b48fcfd720bc999be52
SailenT <david.poletti3@gmail.com>
2021-05-10 19:36:48 +0200

    Poste le basi di una UI

--- src/centrivaccinali/CentriVaccinali.java
+++ src/centrivaccinali/CentriVaccinali.java
@@ -1,12 +1,33 @@
 package centrivaccinali;
+
+import cittadini.Cittadini;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 //TODO METTERE NOME COGNOME MATRICOLA SEDE
 public class CentriVaccinali {
 
 //boia che branch
     public CentriVaccinali(){
+        try {
+            Parent root=FXMLLoader.load(getClass().getResource("SelectionUI.fxml"));
 
-        for(int i=0;i<5;i++){
-            System.out.println("ciao");
+            Scene scene=new Scene(root);
+
+            Stage stage=new Stage();
+
+            stage.setTitle("Seleziona il modulo");
+            stage.setScene(scene);
+            stage.show();
+
+            //scene.lookup();  per prendere un elemento grafico dato il suo ID
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -30,6 +51,13 @@ public class CentriVaccinali {
 
     }
 
+    public void onCentriVaccinaliSelected(){
+        new CentriVaccinaliUI();
+    }
+
+    public void onCittadiniSelected(){
+        new Cittadini();
+    }
 
 
     public static void main(String[] args){
