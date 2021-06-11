762afb7a62d3f95baa502e9611e5d1a0f86896b4
GamerzBros <arduoscri@gmail.com>
2021-06-11 17:03:03 +0200

    Sistemazione Nomenclatura

--- src/centrivaccinali/LoginController.java
+++ src/centrivaccinali/LoginController.java
@@ -2,8 +2,8 @@ package centrivaccinali;
 import javafx.application.*;
 import javafx.stage.Stage;
 
-public class login_controller extends Application {
-    public login_controller(){
+public class LoginController extends Application {
+    public LoginController(){
 
     }
 
