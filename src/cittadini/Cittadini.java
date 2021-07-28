a81a2eb04c095fe8809190e74de9ffa13f2ccef3
Marsio <eros6759@gmail.com>
2021-07-28 15:39:15 +0200

    registra eventi avversi

--- src/cittadini/Cittadini.java
+++ src/cittadini/Cittadini.java
@@ -1,12 +1,32 @@
 package cittadini;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 //TODO METTERE NOME COGNOME MATRICOLA SEDE
 public class Cittadini {
     private SingoloCittadino cittadinoLoggato;
 
     //TODO Marsio: creare il metodo registra eventi avversi
-
+/*
+    public static void registerEventiAvversi()throws Exception {
+        String evento1 = box1.getSelectedItem().toString(); //evento1 = Mal di testa
+        String evento2 = box2.getSelectedItem().toString(); //evento2 = Febbre
+        String evento3 = box3.getSelectedItem().toString(); //evento3 = Dolori muscolari o articolari
+        String evento4 = box4.getSelectedItem().toString(); //evento4 = Linfoadenopatia
+        String evento5 = box5.getSelectedItem().toString(); //evento5 = Tachicardia
+        String evento6 = box6.getSelectedItem().toString(); //evento6 = Crisi ipertensiva
+        FileWriter writer = new FileWriter("account.txt", true);
+        BufferedWriter out = new BufferedWriter(writer);
+        String fileInput =  "Mal di Testa:" + evento1 + ";" + "Febbre:" + evento2 + ";" + "Dolori muscolari o articolari:" + evento3 + ";" + "Linfoadenopatia:" + evento4 + ";" + "Tachicardia:" + evento5 + ";" + "Crisi ipertensiva:" + evento6 + ";";
+        out.write(fileInput);
+        out.newLine();
+        out.flush();
+        out.close();
+    }*/
 
     //TODO creare registrazione e login cittadino
+
     public void registraCittadino(SingoloCittadino cittadino){
 
     }
@@ -14,6 +34,5 @@ public class Cittadini {
     public void loggaCittadini() {
 
     }
-
 
 }
